58687ceb31459aa876fe3aea79ae4a814c7d3926
SungWoo824 <scody1219@gmail.com>
2021-04-30 22:23:18 +0900

    add : poster and description

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -18,13 +18,29 @@ class App extends React.Component {
 
     render() {
         const {isLoading, movies} = this.state;
-        return <div>
-            {isLoading ? "Loading..." : movies.map(movie => {
-                console.log(movie);
-                return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary}
-                              poster={movie.poster}/>
-            })}
-        </div>
+        return (
+            <section className="container">
+                <div>
+                    {isLoading ? (
+                        <div className="loader">
+                            <span className="loader__text">"Loading..."</span>
+                        </div>
+                    ) : (
+                        <div className="movies">
+                            {movies.map(movie => (
+                                <Movie
+                                    key={movie.id}
+                                    id={movie.id}
+                                    year={movie.year}
+                                    title={movie.title}
+                                    summary={movie.summary}
+                                    poster={movie.medium_cover_image}/>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </section>
+        );
     };
 }
 
